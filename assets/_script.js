7a6c339821940f1beda7378134e87174195f4781
Maxim Peretolchin <peretolchinmax@mail.ru>
2020-06-29 19:50:04 +0300

    Slider section and menu made

---
+++ assets/_script.js
@@ -0,0 +1,14 @@
+const openHamburger = document.querySelector("#hamburger");
+const menu = document.querySelector(".sidebar");
+const cross = document.querySelector(".sidebar__close-icon");
+
+openHamburger.addEventListener("click", e => {
+  e.preventDefault();
+  menu.style.display = "flex";
+});
+
+cross.addEventListener("click", e => {
+  e.preventDefault();
+  menu.style.display = "none";
+});
+
