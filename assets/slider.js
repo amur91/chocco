a93bbe40a13c591f8830e9b1f600b36d0da4a5aa
Maxim Peretolchin <peretolchinmax@mail.ru>
2020-07-07 22:30:22 +0300

    form validation

--- assets/slider.js
+++ assets/slider.js
@@ -1,1 +1,17 @@
-$('.slider__display').bxSlider();
+const slider = $('.slider').bxSlider({
+  controls: false,
+  pager: false,
+});
+
+const nextSlide = document.querySelector('.slider__arrow_right');
+const prevSlide = document.querySelector('.slider__arrow_left');
+
+nextSlide.addEventListener('click',(e)=>{
+  e.preventDefault();
+  slider.goToNextSlide();
+});
+
+prevSlide.addEventListener('click',(e)=>{
+  e.preventDefault();
+  slider.goToPrevSlide();
+});
